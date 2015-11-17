5bf54292803bbde5285435642d053803028ef537
Kevin <khuynh10@calstatela.edu>
2015-11-17 00:17:00 -0800

    added entities

---
+++ hwk7/hwk7/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwk7
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base("name=chapter2")
+        {
+
+        }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
+    }
+}
---
+++ hwk7/hwk7/Initializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwk7
+{
+    public class Initializer : DropCreateDatabaseIfModelChanges<Context>
+    {
+        protected override void Seed(Context context)
+        {
+            context.Companies.Add(new Company
+            {
+                Name = "My company"
+            });
+        }
+    }
+}
