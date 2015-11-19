d10144c0ecebaf61466ea27cc8052e92ee273040
Kevin <khuynh10@calstatela.edu>
2015-11-18 20:44:18 -0800

    Added personMap

---
+++ hwk7/hwk7/PersonMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwk7
+{
+    public class PersonMap : EntityTypeConfiguration<Person>
+    {
+        public PersonMap()
+        {
+            Property(p => p.FirstName)
+          .HasMaxLength(30);
+            Property(p => p.LastName)
+                .HasMaxLength(30);
+        }
+    }
+}
